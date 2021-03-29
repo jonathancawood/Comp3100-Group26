a069f63af691b6668d24924ac127e821ceca877c
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-03-29 14:09:58 +1100

    Intial Setup
    Starting to create a client side Simulator

---
+++ Client_Side.java
@@ -0,0 +1,7 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.OutputStream; 
+import java.io.InputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;

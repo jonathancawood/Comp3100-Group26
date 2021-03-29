e1aab95c082c0f2f1044e831b7202c38b1a29b8f
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-03-29 14:43:51 +1100

    Implementing Step 1
    Implementing Step 1 of the protocol
    Client sends HELO to Server

--- Client_Side.java
+++ Client_Side.java
@@ -1,7 +1,36 @@
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.OutputStream; 
 import java.io.InputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+
+public class Client_Side{
+    public static void main(String[] args){
+        try{
+            Socket s = new Socket("localhost", 50000);
+
+            InputStream dis = new DataInputStream(s.getInputStream());
+            OutputStream dout = new DataOutputStream(s.getOutputStream());
+
+            //Make a string, convert it to a byte array then snd to a server 
+            String myString = "HELO";
+            dout.write(myString.getBytes());
+            dout.flush();
+
+            System.out.println("Client has sent \"" + myString + "\" to the server......" );
+
+            //Read the byte stream from the server 
+            byte[] byteArray = new byte[dis.available()];
+            dis.read(byteArray);
+
+            //Make a string using the recieved byte and print the line
+            myString = new String(byteArray, StandardCharsets.UTF_8);
+            System.out.println(myString + "recieved from server");
+
+
+        } catch(Exception e) {System.out.println(e);}
+    }
+
+
+}
